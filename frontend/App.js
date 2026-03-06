5a8da406adf916c27d955223a49153c241f9d3ae
YaznaYne <yassine.zouitni@hotmail.com>
2026-03-06 13:49:22 +0100

    Add online and vs bot game screens with navigation

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,15 @@
 // ./App.js
 import 'react-native-gesture-handler';
 import { SocketContext, socket } from './contexts/socket.context';
+import HomeScreen from './screens/home.screen';
+import OnlineGameScreen from './screens/online-game.screen';
+import VsBotGameScreen from './screens/vs-bot-game.screen';
 
 import React from 'react';
 import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/home.screen';
+
 
 const Stack = createStackNavigator();
 LogBox.ignoreAllLogs(true);
@@ -17,6 +20,8 @@ function App() {
       <NavigationContainer>
         <Stack.Navigator initialRouteName="HomeScreen">
           <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="OnlineGameScreen" component={OnlineGameScreen} />
+          <Stack.Screen name="VsBotGameScreen" component={VsBotGameScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </SocketContext.Provider>
--- frontend/screens/home.screen.js
+++ frontend/screens/home.screen.js
@@ -1,50 +1,26 @@
-import { useEffect, useState, useContext } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { SocketContext } from '../contexts/socket.context';
+// app/screens/home.screen.js
 
-export default function HomeScreen() {
+import { StyleSheet, View, Button } from "react-native";
 
-  const socket = useContext(SocketContext);
-
-  const [time, setTime] = useState(null);
-
-  useEffect(() => {
-
-    socket.on("time-msg", (data) => {
-      setTime(new Date(data.time).toString());
-    });
-
-    return () => {
-      socket.disconnect();
-      socket.removeAllListeners();
-    };
-  }, []);
+export default function HomeScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      {!socket && (
-        <>
-          <Text style={styles.paragraph}>
-            Connecting to websocket server...
-          </Text>
-          <Text style={styles.footnote}>
-            Make sure the backend is started and reachable
-          </Text>
-        </>
-      )}
-
-      {socket && (
-        <>
-          <Text style={[styles.paragraph, { fontWeight: "bold" }]}>
-            Server time
-          </Text>
-          <Text style={styles.paragraph}>{time}</Text>
-        </>
-      )}
+      <View>
+        <Button
+          title="Jouer en ligne"
+          onPress={() => navigation.navigate('OnlineGameScreen')}
+        />
+      </View>
+      <View>
+        <Button
+          title="Jouer contre le bot"
+          onPress={() => navigation.navigate('VsBotGameScreen')}
+        />
+      </View>
     </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -52,12 +28,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  paragraph: {
-    fontSize: 16,
-  },
-  footnote: {
-    fontSize: 14,
-    fontStyle: "italic",
-  },
+  }
 });
